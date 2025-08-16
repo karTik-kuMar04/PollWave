018116454d98e8a9246292405853bbc5fb55519c
karTik-kuMar04 <mysoulisinfinity1@gmail.com>
2025-08-16 09:58:29 +0530

    Fix route definitions for polls and quizzes, ensuring proper JWT verification

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { register, login, getMe } from "../controller/user.controller.js";
+import { createPoll, createQuiz, respondToPoll, attemptQuiz } from "../controller/host.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js"; // we'll add a simple one below
 
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", verifyJWT, getMe);
+
+
+router.post("/polls)", verifyJWT, createPoll);
+router.post("/quizzes", verifyJWT, createQuiz);
+
+router.post("/polls/:pollId/respond", verifyJWT, respondToPoll);
+router.post("/quizzes/:quizId/respond", verifyJWT, attemptQuiz);
+
 
 export default router;
