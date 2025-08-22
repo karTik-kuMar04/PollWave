daa60508223d6b8b31df741c99e4f46ba738bd84
karTik-kuMar04 <mysoulisinfinity1@gmail.com>
2025-08-22 16:34:48 +0530

    Refactor API route setup; remove user-specific prefix for cleaner routing

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 import router from "./routes/route.js";
-app.use("/api/v1/users", router);
+app.use(router);
 
 connectDB().then(() => {
   app.listen(process.env.PORT || 4000, () => console.log(`Server is running at port : ${process.env.PORT}`));
